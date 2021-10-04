7375597bf80fa274c9464fc0c36af1eaa8c625ec
evan-winata <evan.winata@lendlease.com>
2021-10-04 18:54:11 +1100

    code cleanup

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,8 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
 import shapeReducer from '../slices/shape/shapeSlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     shape: shapeReducer,
   },
 });
